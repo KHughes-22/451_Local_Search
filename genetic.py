686d3cc2e404640f9120335338a0df018eda4968
Kyle Hughes <48317955+KHughes-22@users.noreply.github.com>
2024-09-17 13:41:17 -0700

    commit

--- genetic.py
+++ genetic.py
@@ -11,7 +11,7 @@ param int queens: the max number that the chrom can be. The crosspoint is random
 return: returns two strings that have been mutated together at the crosspoint
 """
 def cross(str1, str2, queens):
-    crosspoint = random.randomrange(1,queens - 1)
+    crosspoint = random.randrange(1,queens)
     return str1[:crosspoint] + str2[crosspoint:], str2[:crosspoint] + str1[crosspoint:]
 
 
@@ -41,18 +41,31 @@ def main():
         percentage_list.append(element / total_fitness)
 
 
-    # print("Chrom List: ", chrom_list)
-    # print("fitness_list: ", fitness_list)
-    # print("Fitness total: ", total_fitness)
-    # print("Percentage_list: ", percentage_list)
+    print("Chrom List: ", chrom_list)
+    print("fitness_list: ", fitness_list)
+    print("Fitness total: ", total_fitness)
+    print("Percentage_list: ", percentage_list)
     # print("pair selection")
     #print(random.choice(chrom_list, weights=percentage_list))
     pair = []
-    print(type(random.choices(chrom_list, weights=percentage_list)))
+    index = 0
     while(chrom_list):
         temp = random.choices(chrom_list, weights=percentage_list)
-        pair.append(temp)
-        chrom_list.remove(temp)
+        #print(temp[0])
+        for i in range(len(chrom_list)):
+            #print(i)
+            #print(temp[0], chrom_list[i])
+            if temp[0] == chrom_list[i]:
+                code = chrom_list.pop(i)
+                pair.append(code)
+                percentage_list.pop(i)
+                print(i)
+                break
+    print(pair)
+    pairs = len(pair) / 2
+    print(pairs)
+    for i in range(pairs):
+        pair[i], pair[i + 1] = cross(pair[0], pair[1], queens)
     print(pair)
 
     
